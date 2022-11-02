4e83bcbee4fa796b78aeff0f81fb45ae3bbb9f79
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-11-01 20:18:08 -0500

    commit tree 0cef2670f961cd190437233147c8d2c70644c940

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    let newArr = drivers.slice(0,2);
+    //console.log(drivers.slice(0, 2));
+    return newArr
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    let newArr = drivers.slice(-2);
+    return newArr
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+    function fareMultiplier(fare) {
+    return fare * multiplier
+    }
+    return fareMultiplier;
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2
+}
+
+const fareTripler = (fare) => {
+    return fare *3
+}
+
+function selectDifferentDrivers(drivers, secondArgument) {
+    let arr1 = drivers.slice(0,2)
+    let arr2 = drivers.slice(-2)
+    if (secondArgument === returnFirstTwoDrivers) {
+        return arr1;
+    } else if (secondArgument === returnLastTwoDrivers) {
+        return arr2
+    }
+    else {
+        return undefined
+    }
+
+}
